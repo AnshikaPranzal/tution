28c796b66e59c2588388db9523a30910945937d0
prodiptahalder <59222228+prodiptahalder@users.noreply.github.com>
2020-07-30 03:44:30 -0700

    Course linked with Subject model

--- src/components/AllCourseList.js
+++ src/components/AllCourseList.js
@@ -4,7 +4,7 @@ import course_pic1  from '../images/courses/physics2.png';
 import course_pic2  from '../images/courses/chemistry3.png';
 import course_pic3  from '../images/courses/maths.png';
 
-import { isAuthenticated,addItemToCart,loadCart } from './helper';
+import { isAuthenticated,addItemToCart,loadCart,getAllSubjects } from './helper';
 
 
 const AllCourseList = () => {
@@ -24,7 +24,24 @@ const AllCourseList = () => {
 
       }
     }
-  
+    const [subject, setsubject] = useState([])
+    const [errorF, seterrorF] =useState(false)
+
+    const loadAllSubjects = () =>{
+        getAllSubjects().then(data =>{
+          //   console.log(data)
+          if(data)
+          if(data.error){
+            seterrorF(data.error)
+          }
+          else{
+            setsubject(data)
+          }
+        })
+      }
+      useEffect (() => {
+        loadAllSubjects()
+        },[])
   
 console.log("jkkkk",product)
     return(
@@ -32,14 +49,18 @@ console.log("jkkkk",product)
               <section className="section">
               <div className='container'>
             <div className="row justify-content-center">
-            <CourseItem image={course_pic1} date="02-14-2018" product={product} subject="Science" topic="Physics" des=" Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+              {subject.map((obj,i) => {
+                return(<CourseItem key={i} subjectid={obj._id}  product={product} price={obj.price}  topic={obj.name} des=" Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna."></CourseItem>)
+              })}
+            {/* <CourseItem image={course_pic1} date="02-14-2018" product={product} subject="Science" topic="Physics" des=" Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
           incididunt ut labore et dolore magna."></CourseItem>
                 <CourseItem image={course_pic2} date="02-14-2018" product={product} subject="Science" topic="Chemistry" des=" Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
           incididunt ut labore et dolore magna."></CourseItem>
                 <CourseItem image={course_pic3} date="02-14-2018" product={product} subject="Science" topic="Mathematics" des=" Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
           incididunt ut labore et dolore magna."></CourseItem>
           <CourseItem image={course_pic3} date="02-14-2018" product={product} subject="Science" topic="Biology" des=" Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-          incididunt ut labore et dolore magna."></CourseItem>
+          incididunt ut labore et dolore magna."></CourseItem> */}
             </div>
             </div>
             </section>
--- src/components/CourseItem.js
+++ src/components/CourseItem.js
@@ -75,16 +75,18 @@ const CourseItem = (props) => {
         <React.Fragment>
              <div className="col-lg-3 col-sm-6 mb-5" >
     <div className="card p-0 border-primary rounded-0 hover-shadow" style={{overflow: "hidden"}}>
-      <img className="card-img-top rounded-0" src={props.image} alt="course thumb" style={{height: "15rem", width: "15rem", alignSelf: "center"}}></img>
+      {/* <img className="card-img-top rounded-0" src={props.image} alt="course thumb" style={{height: "15rem", width: "15rem", alignSelf: "center"}}></img> */}
       <div className="card-body">
         <ul className="list-inline mb-2">
-          <li className="list-inline-item"><i className="ti-calendar mr-1 text-color"></i>{props.date}</li>
-          <li className="list-inline-item">{props.subject}</li>
+          {/* <li className="list-inline-item"><i className="ti-calendar mr-1 text-color"></i>{props.date}</li> */}
+          <li className="list-inline-item"></li>
         </ul>
         <a href="course-single.html">
           <h4 className="card-title">{props.topic}</h4>
         </a>
         <p className="card-text mb-4"> {props.des}</p>
+        <p className="list-inline-item">Rs.{props.price}</p>
+        <hr></hr>
         <a  data-toggle="modal" data-target="#signinModal" className="hvr-bounce-to-top" topic={props.topic} onClick={addProductToCart}>Enroll</a>
       </div>
     </div>
