3e5fd5a99c21b29b3577c8df2510af2c4644b000
prodiptahalder <59222228+prodiptahalder@users.noreply.github.com>
2020-07-31 08:34:52 -0700

    update

--- src/components/Dashboard/views/starter/starter.jsx
+++ src/components/Dashboard/views/starter/starter.jsx
@@ -32,50 +32,6 @@ const Starter = () => {
                     <Projects />
                 </Col>
             </Row>
-            <Row>
-                <Col xs="12" md="4">
-                    {/*--------------------------------------------------------------------------------*/}
-                    {/*Card-1*/}
-                    {/*--------------------------------------------------------------------------------*/}
-                    <Card>
-                        <CardImg top width="100%" src={img1} />
-                        <CardBody>
-                            <CardTitle>Card title</CardTitle>
-                            <CardSubtitle>Card subtitle</CardSubtitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                            <Button>Button</Button>
-                        </CardBody>
-                    </Card>
-                </Col>
-                <Col xs="12" md="4">
-                    {/*--------------------------------------------------------------------------------*/}
-                    {/*Card-1*/}
-                    {/*--------------------------------------------------------------------------------*/}
-                    <Card>
-                        <CardImg top width="100%" src={img2} />
-                        <CardBody>
-                            <CardTitle>Card title</CardTitle>
-                            <CardSubtitle>Card subtitle</CardSubtitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                            <Button>Button</Button>
-                        </CardBody>
-                    </Card>
-                </Col>
-                <Col xs="12" md="4">
-                    {/*--------------------------------------------------------------------------------*/}
-                    {/*Card-1*/}
-                    {/*--------------------------------------------------------------------------------*/}
-                    <Card>
-                        <CardImg top width="100%" src={img3} />
-                        <CardBody>
-                            <CardTitle>Card title</CardTitle>
-                            <CardSubtitle>Card subtitle</CardSubtitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                            <Button>Button</Button>
-                        </CardBody>
-                    </Card>
-                </Col>
-            </Row>
         </div>
     );
 }
