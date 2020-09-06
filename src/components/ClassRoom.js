791a3db7a95d479c7b40405aebdc4a330329bd39
prodiptahalder <59222228+prodiptahalder@users.noreply.github.com>
2020-09-06 06:53:26 -0700

    Classroom Update

--- src/components/ClassRoom.js
+++ src/components/ClassRoom.js
@@ -1,148 +1,165 @@
 /* eslint-disable no-unused-vars */
-import React,{useState, useEffect} from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import {
-    Card,
-    CardBody,
-    CardTitle,
-    CardSubtitle,
-    Button,
-    Row,
-    Col,
-    Input,
-    Table
-} from 'reactstrap';
-import $ from 'jquery'
-import TopHeader from './TopHeader.js';
-import Navbar from './Navbar';
-import ClassroomTitle from './ClassroomTitle'
-import UploadDocument from './UploadDocument';
-import UploadAssignment from './UploadAssignment';
-import {getAClassroom, getAllUSers, updateClassroom,deleteClassroom,isAuthenticated} from './helper/index';
-
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Row,
+  Col,
+  Input,
+  Table,
+} from "reactstrap";
+import $ from "jquery";
+import TopHeader from "./TopHeader.js";
+import Navbar from "./Navbar";
+import ClassroomTitle from "./ClassroomTitle";
+import UploadDocument from "./UploadDocument";
+import UploadAssignment from "./UploadAssignment";
+import {
+  getAClassroom,
+  getAllUSers,
+  updateClassroom,
+  deleteClassroom,
+  isAuthenticated,
+} from "./helper/index";
 
 const ClassRoom = (props) => {
-    console.log(isAuthenticated())
+  console.log(isAuthenticated());
 
-    // Sticky Menu
-	$(window).scroll(function () {
-		var height = 0;
-		if ($('header').offset().top > 10) {
-			$('.top-header').addClass('hide');
-			$('.navigation').addClass('nav-bg');
-			$('.navigation').css('margin-top', '-' + height + 'px');
-		} else {
-			$('.top-header').removeClass('hide');
-			$('.navigation').removeClass('nav-bg');
-			$('.navigation').css('margin-top', '-' + 0 + 'px');
-		}
+  // Sticky Menu
+  $(window).scroll(function () {
+    var height = 0;
+    if ($("header").offset().top > 10) {
+      $(".top-header").addClass("hide");
+      $(".navigation").addClass("nav-bg");
+      $(".navigation").css("margin-top", "-" + height + "px");
+    } else {
+      $(".top-header").removeClass("hide");
+      $(".navigation").removeClass("nav-bg");
+      $(".navigation").css("margin-top", "-" + 0 + "px");
+    }
+  });
+  const crid = props.match.params.handle;
+  const Month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const [project, setProject] = useState({
+    name: "",
+    description: "",
+    subject: "",
+    standard: "",
+    error: "",
+    members: [],
+    doc: [],
+    success: false,
+  });
+  const [refresh, setrefresh] = useState(true);
+  const [userO, setuserO] = useState([]);
+  const [errorF, seterrorF] = useState(false);
+  var students = 0;
+  var studentsarray = [];
+
+  const loadAllusers = () => {
+    getAllUSers().then((data) => {
+      console.log(data);
+      if (data)
+        if (data.error) {
+          seterrorF(data.error);
+        } else {
+          setuserO(data);
+        }
     });
-    const crid= props.match.params.handle;
-    const Month = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+  };
 
-    const [project, setProject] = useState({
-        name: "",
-        description: "",
-        subject: "",
-        standard: "",
-        error:"",
-        members:[],
-        doc:[],
-        success: false
-    })
-    const [refresh, setrefresh] = useState(true)
-    const [userO, setuserO] =useState([])
-    const [errorF, seterrorF] = useState(false)
-    var students = 0
-    var studentsarray = []
+  useEffect(() => {
+    loadAllusers();
+  }, []);
 
-    const loadAllusers = () =>{
-        getAllUSers().then(data =>{
-            console.log(data)
-          if(data)
-          if(data.error){
-            seterrorF(data.error)
-          }
-          else{
-            setuserO(data)
-            
-            
-          }
-        })
+  const getClassroom = (cid) => {
+    getAClassroom(cid).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+        // setValues({...values,error:data.error})
+      } else {
+        setProject({
+          ...project,
+          name: data.name,
+          description: data.description,
+          subject: data.subject,
+          standard: data.standard,
+          members: data.members,
+          doc: data.doc,
+          assignment: data.assignment,
+        });
+        setrefresh(false);
       }
+    });
+  };
 
-      useEffect (() => {
-        loadAllusers()
-        },[])
+  useEffect(() => {
+    getClassroom(crid);
+  }, [refresh]);
 
-    const getClassroom = cid => {
-        getAClassroom(cid).then(data=>{
-            
-            if(data.error)
-            {
-                console.log(data.error)
-                // setValues({...values,error:data.error})
-            }
-            else{
-                setProject({
-                    ...project,
-                    name: data.name,
-                    description: data.description,
-                    subject: data.subject,
-                    standard: data.standard,
-                    members: data.members,
-                    doc:data.doc,
-                    assignment: data.assignment,
-                });
-                setrefresh(false);
-            }
-        })}
-
-        useEffect (() => {
-            getClassroom(crid)
-            },[refresh])
-
-    return(
+  return (
     <React.Fragment>
-         <header className="fixed-top header">
-            {/* <!-- navbar --> */}
-			<Navbar></Navbar>
-          </header>
-          {/* <!-- /header --> */}
-          <ClassroomTitle title={project.name} body={project.description}></ClassroomTitle>
-          <div className="text-center">
-          <Row style={{width: "100vw", margin: "0"}}>
-          
-                        {/* <CardImg top width="100%" src={img2} /> */}
-                            {isAuthenticated() 
-                            ?
-                            <Col xs="12" md="12">
-                            {/*--------------------------------------------------------------------------------*/}
-                            {/*Card-1*/}
-                            {/*--------------------------------------------------------------------------------*/}
-                            <Card>
-                            <CardBody>
-                            <UploadDocument id={crid}></UploadDocument>
-                        </CardBody>
-                        </Card>
-                        </Col>
-                        :""}
+      <header className="fixed-top header">
+        {/* <!-- navbar --> */}
+        <Navbar></Navbar>
+      </header>
+      {/* <!-- /header --> */}
+      <ClassroomTitle
+        title={project.name}
+        body={project.description}
+      ></ClassroomTitle>
+      <div className="text-center">
+        <Row style={{ width: "100vw", margin: "0" }}>
+          {/* <CardImg top width="100%" src={img2} /> */}
+          {isAuthenticated() ? (
+            <Col xs="12" md="12">
+              {/*--------------------------------------------------------------------------------*/}
+              {/*Card-1*/}
+              {/*--------------------------------------------------------------------------------*/}
+              <Card>
+                <CardBody>
+                  <UploadDocument id={crid}></UploadDocument>
+                </CardBody>
+              </Card>
+            </Col>
+          ) : (
+            ""
+          )}
 
-                        {isAuthenticated() 
-                            ?
-                            <Col xs="12" md="12">
-                            {/*--------------------------------------------------------------------------------*/}
-                            {/*Card-1*/}
-                            {/*--------------------------------------------------------------------------------*/}
-                            <Card style={{overflowX: "scroll"}}>
-                            <CardBody>
-                            <UploadAssignment id={crid}></UploadAssignment>
-                        </CardBody>
-                        </Card>
-                        </Col>
-                        :""}
+          {isAuthenticated() ? (
+            <Col xs="12" md="12">
+              {/*--------------------------------------------------------------------------------*/}
+              {/*Card-1*/}
+              {/*--------------------------------------------------------------------------------*/}
+              <Card style={{ overflowX: "scroll" }}>
+                <CardBody>
+                  <UploadAssignment id={crid}></UploadAssignment>
+                </CardBody>
+              </Card>
+            </Col>
+          ) : (
+            ""
+          )}
 
-                        {
+          {/* {
                             userO.map((obj,i) => {
                                 if(obj.standard === undefined){
                                     obj.standard = 0;
@@ -167,48 +184,42 @@ const ClassRoom = (props) => {
                                 }
                             })
                         
-                        }
-                        {isAuthenticated().user.role === 1
-                            ?
-                            <Col xs="12" md="12">
-                            {/*--------------------------------------------------------------------------------*/}
-                            {/*Card-1*/}
-                            {/*--------------------------------------------------------------------------------*/}
-                            <Card style={{overflowX: "scroll"}}>
-                            <CardBody>
-                            <h2>Member List</h2>
-                            <Table>
-                                <th>Names</th>
-                                <th>Standard</th>
-                                <th>Contact</th>
-                        {
-                        studentsarray.map((obj,i)=>{
-                            return(
-                            // <tr key={i}>
-                            
-                                <tr key={i}>
+                        } */}
+          {isAuthenticated().user.role === 0 ? (
+            <Col xs="12" md="12">
+              {/*--------------------------------------------------------------------------------*/}
+              {/*Card-1*/}
+              {/*--------------------------------------------------------------------------------*/}
+              <Card style={{ overflowX: "scroll" }}>
+                <CardBody>
+                  <h2>Member List</h2>
+                  <Table>
+                    <th>Names</th>
+                    <th>Standard</th>
+                    <th>Contact</th>
+                    {project.members.map((obj, i) => {
+                      return (
+                        // <tr key={i}>
 
-                                        <td>{obj.name}</td>
-                                        <td>{obj.standard}</td>
-                                        <td>{obj.mob}</td>
-                                        {/* <CardSubtitle>{obj.subject}</CardSubtitle>
+                        <tr key={i}>
+                          <td>{obj.name}</td>
+                          <td>{obj.standard}</td>
+                          <td>{obj.mob}</td>
+                          {/* <CardSubtitle>{obj.subject}</CardSubtitle>
                                         <CardBody>{obj.description}</CardBody> */}
-
-                                </tr>
-                                )
-                           
-                        
+                        </tr>
+                      );
                     })}
-                        </Table>
-
-                        </CardBody>
-                        </Card>
-                        </Col>
-                        :""}
-                        
-                        </Row>
-
-                        </div>
-    </React.Fragment>)
-}
+                  </Table>
+                </CardBody>
+              </Card>
+            </Col>
+          ) : (
+            ""
+          )}
+        </Row>
+      </div>
+    </React.Fragment>
+  );
+};
 export default ClassRoom;
