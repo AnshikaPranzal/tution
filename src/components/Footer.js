b03f9ac3db807d6f89612a56b37b2e6008066f8c
Rahul Mahato <rahulm@geekyants.com>
2020-09-10 02:10:04 +0530

    minor fixes vmin to rem

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,56 +1,56 @@
-import React from "react";
-import logo from "../images/logo.png";
-import { Link } from "react-router-dom";
+import React from 'react';
+import logo from '../images/logo.png';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <React.Fragment>
       <footer>
-        <div className="newsletter">
-          <div className="container1">
-            <div className="row">
-              <div className="col-md-9 ml-auto bg-primary py-5 newsletter-block">
-                <h3 className="text-white">Subscribe Now</h3>
-                <form action="#">
-                  <div className="input-wrapper">
+        <div className='newsletter'>
+          <div className='bg-primary '>
+            <div className='row'>
+              <div className='col-md-5 mx-auto py-5 newsletter-block'>
+                <h3 className='text-white'>Subscribe Now</h3>
+                <form action='#' className='text-center'>
+                  <div className='input-wrapper'>
                     <input
-                      type="email"
-                      className="form-control border-0"
-                      id="newsletter"
-                      name="newsletter"
-                      placeholder="Enter Your Email..."
+                      type='email'
+                      className='form-control border-0'
+                      id='newsletter'
+                      name='newsletter'
+                      placeholder='Enter Your Email...'
                     ></input>
-                    <button
-                      type="submit"
-                      value="send"
-                      className="hvr-bounce-to-top"
-                    >
-                      Join
-                    </button>
                   </div>
+                  <button
+                    type='submit'
+                    value='send'
+                    className='hvr-bounce-to-top my-3 border-curved'
+                  >
+                    Join
+                  </button>
                 </form>
               </div>
             </div>
           </div>
         </div>
         {/* <!-- footer content --> */}
-        <div className="newsfooter bg-footer section border-bottom">
-          <div className="container1">
-            <div className="row">
-              <div className="col-lg-6 col-sm-8 mb-5 mb-lg-0">
+        <div className='newsfooter bg-footer section border-bottom'>
+          <div className='container1'>
+            <div className='row'>
+              <div className='col-lg-6 col-sm-8 mb-5 mb-lg-0'>
                 {/* <!-- logo --> */}
-                <Link className="logo-footer" to="/">
+                <Link className='logo-footer' to='/'>
                   <img
-                    className="img-fluid mb-4"
+                    className='img-fluid mb-4'
                     src={logo}
-                    alt="logo"
-                    style={{ paddingTop: "25%" }}
+                    alt='logo'
+                    style={{ paddingTop: '25%' }}
                   ></img>
                 </Link>
               </div>
-              <div className="col-lg-4 col-sm-8 mb-5 mb-lg-0">
+              <div className='col-lg-4 col-sm-8 mb-5 mb-lg-0'>
                 {/* <!-- About Us --> */}
-                <h4 className="text-white mb-5">About Us</h4>
+                <h4 className='text-white mb-5'>About Us</h4>
                 <p>
                   Our courses offer a good compromise between the continuous
                   assessment favoured by some universities and the emphasis
@@ -68,16 +68,16 @@ const Footer = () => {
                     </ul>
                     </div> */}
               {/* <!-- Reach --> */}
-              <div className="col-lg-2 col-md-3 col-sm-4 col-6 mb-5 mb-md-0">
-                <h4 className="text-white mb-5">REACH US</h4>
-                <ul className="list-unstyled">
-                  <ul className="list-unstyled">
-                    <li className="mb-2">
+              <div className='col-lg-2 col-md-3 col-sm-4 col-6 mb-5 mb-md-0'>
+                <h4 className='text-white mb-5'>REACH US</h4>
+                <ul className='list-unstyled'>
+                  <ul className='list-unstyled'>
+                    <li className='mb-2'>
                       23621 15 Mile Rd #C104, Clinton MI, 48035, New York, USA
                     </li>
-                    <li className="mb-2">+1 (2) 345 6789</li>
-                    <li className="mb-2">+1 (2) 345 6789</li>
-                    <li className="mb-2">contact@yourdomain.com</li>
+                    <li className='mb-2'>+1 (2) 345 6789</li>
+                    <li className='mb-2'>+1 (2) 345 6789</li>
+                    <li className='mb-2'>contact@yourdomain.com</li>
                   </ul>
                 </ul>
               </div>
@@ -95,54 +95,54 @@ const Footer = () => {
           </div>
         </div>
         {/* <!-- copyright --> */}
-        <div className="copyright py-4 bg-footer">
-          <div className="container1">
-            <div className="row">
-              <div className="col-sm-4 text-sm-left text-center">
-                <p className="mb-0">
+        <div className='copyright py-4 bg-footer'>
+          <div className='container1'>
+            <div className='row'>
+              <div className='col-sm-4 text-sm-left text-center'>
+                <p className='mb-0'>
                   Copyright
                   <script>
                     var CurrentYear = new Date().getFullYear()
                     document.write(CurrentYear)
                   </script>
-                  ©{" "}
+                  ©{' '}
                 </p>
               </div>
-              <div className="col-sm-4 text-sm-right text-center">
-                <ul className="list-inline">
+              <div className='col-sm-4 text-sm-right text-center'>
+                <ul className='list-inline'>
                   {/* <li className="list-inline-item"><Link className="d-inline-block p-2" to='/course'>Course</Link></li> */}
-                  <li className="list-inline-item">
-                    <Link className="d-inline-block p-2" to="/about">
+                  <li className='list-inline-item'>
+                    <Link className='d-inline-block p-2' to='/about'>
                       About
                     </Link>
                   </li>
-                  <li className="list-inline-item">
-                    <Link className="d-inline-block p-2" to="/contact">
+                  <li className='list-inline-item'>
+                    <Link className='d-inline-block p-2' to='/contact'>
                       Contact
                     </Link>
                   </li>
                 </ul>
               </div>
-              <div className="col-sm-4 text-sm-right text-center">
-                <ul className="list-inline">
-                  <li className="list-inline-item">
-                    <a className="d-inline-block p-2" href="/">
-                      <i className="ti-facebook text-primary"></i>
+              <div className='col-sm-4 text-sm-right text-center'>
+                <ul className='list-inline'>
+                  <li className='list-inline-item'>
+                    <a className='d-inline-block p-2' href='/'>
+                      <i className='ti-facebook text-primary'></i>
                     </a>
                   </li>
-                  <li className="list-inline-item">
-                    <a className="d-inline-block p-2" href="/">
-                      <i className="ti-twitter-alt text-primary"></i>
+                  <li className='list-inline-item'>
+                    <a className='d-inline-block p-2' href='/'>
+                      <i className='ti-twitter-alt text-primary'></i>
                     </a>
                   </li>
-                  <li className="list-inline-item">
-                    <a className="d-inline-block p-2" href="/">
-                      <i className="ti-linkedin text-primary"></i>
+                  <li className='list-inline-item'>
+                    <a className='d-inline-block p-2' href='/'>
+                      <i className='ti-linkedin text-primary'></i>
                     </a>
                   </li>
-                  <li className="list-inline-item">
-                    <a className="d-inline-block p-2" href="/">
-                      <i className="ti-instagram text-primary"></i>
+                  <li className='list-inline-item'>
+                    <a className='d-inline-block p-2' href='/'>
+                      <i className='ti-instagram text-primary'></i>
                     </a>
                   </li>
                 </ul>
--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import '../plugins/bootstrap/bootstrap.min.css'
-import '../plugins/slick/slick.css'
-import '../plugins/themify-icons/themify-icons.css'
-import '../plugins/animate/animate.css'
-import '../plugins/aos/aos.css'
-import '../plugins/venobox/venobox.css'
-import '../css/style.css'
+import '../plugins/bootstrap/bootstrap.min.css';
+import '../plugins/slick/slick.css';
+import '../plugins/themify-icons/themify-icons.css';
+import '../plugins/animate/animate.css';
+import '../plugins/aos/aos.css';
+import '../plugins/venobox/venobox.css';
+import '../css/style.css';
 import { Link } from 'react-router-dom';
 
 const Slide = (props) => {
-
-
-
-
-    return(
-        <React.Fragment>
-        <div className="hero-slider-item">
-        <div className="row">
-          <div className="col-md-8">
-           <h1 className="text-white"  style={{fontSize: '7vmin'}}>{props.heading}</h1>
-            <p className="text-muted mb-4" >{props.body}</p>
-            <Link to='/contact' className=" hvr-bounce-to-top" >Contact Us</Link>
+  return (
+    <React.Fragment>
+      <div className='hero-slider-item pt-5'>
+        <div className='row'>
+          <div className='col-md-8'>
+            <h1 className='text-white' style={{ fontSize: '7vmin' }}>
+              {props.heading}
+            </h1>
+            <p className='text-muted mb-4'>{props.body}</p>
+            <Link to='/contact' className=' hvr-bounce-to-top'>
+              Contact Us
+            </Link>
           </div>
         </div>
       </div>
-        </React.Fragment>
-    )
-}
+    </React.Fragment>
+  );
+};
 
 export default Slide;
