23200a1411b247b124f6e1f771719311b7d9cf43
AnshikaPranzal <1829041@kiit.ac.in>
2020-09-10 14:31:02 +0530

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className='newsletter'>
           <div className='bg-primary '>
             <div className='row'>
-              <div className='col-md-5 mx-auto py-5 newsletter-block'>
+              <div className='col-md-3 mx-auto py-5 newsletter-block'>
                 <h3 className='text-white'>Subscribe Now</h3>
                 <form action='#' className='text-center'>
                   <div className='input-wrapper'>
@@ -37,60 +37,44 @@ const Footer = () => {
         <div className='newsfooter bg-footer section border-bottom'>
           <div className='container1'>
             <div className='row'>
-              <div className='col-lg-6 col-sm-8 mb-5 mb-lg-0'>
+              <div className='col-lg-4 col-sm-8 mb-5 mb-lg-0' style={{margin:"auto",marginLeft:"0px"}}>
                 {/* <!-- logo --> */}
                 <Link className='logo-footer' to='/'>
                   <img
                     className='img-fluid mb-4'
                     src={logo}
                     alt='logo'
-                    style={{ paddingTop: '25%' }}
+                    // style={{ paddingTop: '25%' }}
                   ></img>
                 </Link>
               </div>
-              <div className='col-lg-4 col-sm-8 mb-5 mb-lg-0'>
+              <div className='col-lg-3 col-sm-8 mb-5 mb-lg-0'>
                 {/* <!-- About Us --> */}
-                <h4 className='text-white mb-5'>About Us</h4>
-                <p>
+                <h3 className='text-white mb-5'>About Us</h3>
+                <p className="footer-text">
                   Our courses offer a good compromise between the continuous
                   assessment favoured by some universities and the emphasis
                   placed on final exams by others.
                 </p>
               </div>
-              {/* <!-- company --> */}
-              {/* <div className="col-lg-2 col-md-3 col-sm-4 col-6 mb-5 mb-md-0">
-                    <h4 className="text-white mb-5">LINK</h4>
-                    <ul className="list-unstyled">
-                        <li className="mb-3"><Link className="text-color" to='/course'>Home</Link></li>
-                        <li className="mb-3"><Link className="text-color" to='/course'>Courses</Link></li>
-                        <li className="mb-3"><Link className="text-color" to='/about'>About Us</Link></li>
-                        <li className="mb-3"><Link className="text-color" to='/contact'>Contact</Link></li>
-                    </ul>
-                    </div> */}
+            
               {/* <!-- Reach --> */}
-              <div className='col-lg-2 col-md-3 col-sm-4 col-6 mb-5 mb-md-0'>
-                <h4 className='text-white mb-5'>REACH US</h4>
-                <ul className='list-unstyled'>
+              <div className='col-lg-3 col-md-3 col-sm-4 col-6 mb-5 mb-md-0'>
+                <h3 className='text-white mb-5'>Reach us</h3>
+                
+                  <ul className='list-unstyled footer-text'>
                   <ul className='list-unstyled'>
                     <li className='mb-2'>
-                      23621 15 Mile Rd #C104, Clinton MI, 48035, New York, USA
+                      20khb, Kolkata, Kolkata,West Bengal
                     </li>
                     <li className='mb-2'>+1 (2) 345 6789</li>
                     <li className='mb-2'>+1 (2) 345 6789</li>
                     <li className='mb-2'>contact@yourdomain.com</li>
                   </ul>
                 </ul>
+                
               </div>
-              {/* <!-- support --> */}
-              {/* <div className="col-lg-2 col-md-3 col-sm-4 col-6 mb-5 mb-md-0">
-                    <h4 className="text-white mb-5">SUPPORT</h4>
-                    <ul className="list-unstyled">
-                        <li className="mb-3"><a className="text-color" href="/">Forums</a></li>
-                        <li className="mb-3"><a className="text-color" href="/">Documentation</a></li>
-                        <li className="mb-3"><a className="text-color" href="/">Language</a></li>
-                        <li className="mb-3"><a className="text-color" href="/">Release Status</a></li>
-                    </ul>
-                    </div> */}
+              
             </div>
           </div>
         </div>
@@ -99,7 +83,7 @@ const Footer = () => {
           <div className="container1">
             <div className="row">
               <div className="col-sm-4 text-sm-left text-center">
-                <p className="mb-0">
+                <p className="mb-0 footer-text">
                   Designed and maintained by{" "}
                   {/* <script>
                     var CurrentYear = new Date().getFullYear()
